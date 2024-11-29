1dbfdb4d222c2f7f9e5e53be25a76f8c8978cd0e
Giacomo Marinangeli <31297+jibbolo@users.noreply.github.com>
2024-11-29 14:38:30 +0100

    fix: build config for cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ["@rollup/rollup-linux-x64-gnu"],
+      },
+    },
+  },
 });
