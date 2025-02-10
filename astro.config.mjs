67bd7b08c29fe5d063e3c85c68ac9afce6083a4d
Giacomo Marinangeli <31297+jibbolo@users.noreply.github.com>
2025-02-10 18:50:02 +0100

    feat: playing with tailscale v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   vite: {
+    plugins: [tailwindcss()],
     build: {
       rollupOptions: {
         external: ["@rollup/rollup-linux-x64-gnu"],
